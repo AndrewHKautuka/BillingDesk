29ea36f1b46b306e699678205bae3d7c545ce5a9
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2026-06-26 19:54:58 +0200

    Add Subscription DbSet and configure default status
    - Add Subscription DbSet to BillingDeskDbContext for database access
    - Import SubscriptionStatus enum from subscription domain
    - Configure Subscription entity with default Status value of Active
    - Improves data persistence layer with subscription entity mapping

--- BillingDesk-API/BillingDesk/Common/BillingDeskDbContext.cs
+++ BillingDesk-API/BillingDesk/Common/BillingDeskDbContext.cs
@@ -1,4 +1,5 @@
 using BillingDesk.Common.Constants;
+using BillingDesk.Subscription.Types.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace BillingDesk.Common;
@@ -6,9 +7,16 @@ namespace BillingDesk.Common;
 public class BillingDeskDbContext(DbContextOptions<BillingDeskDbContext> options)
 	: DbContext(options)
 {
+	public DbSet<Subscription.Types.Models.Subscription> Subscription { get; set; } = null!;
+
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		modelBuilder.HasDefaultSchema(DbContextConstants.DatabaseSchema);
+
 		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<Subscription.Types.Models.Subscription>()
+					.Property(e => e.Status)
+					.HasDefaultValue(SubscriptionStatus.Active);
 	}
 }
