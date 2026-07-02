d1eb883929616f5b728caae508a934172adc378d
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2026-07-02 14:31:25 +0200

    Remove data annotations and simplify model
    - Remove System.ComponentModel.DataAnnotations using statement
    - Remove [StringLength(64)] attributes from Name and Category properties
    - Add ReSharper disable comment for EntityFramework.ModelValidation.UnlimitedStringLength
    - Consolidate property declarations by removing unnecessary blank lines
    - Simplify model to not have unnecessary constraints

--- BillingDesk-API/BillingDesk/Subscription/Types/Models/Subscription.cs
+++ BillingDesk-API/BillingDesk/Subscription/Types/Models/Subscription.cs
@@ -1,4 +1,5 @@
-using System.ComponentModel.DataAnnotations;
+// ReSharper disable EntityFramework.ModelValidation.UnlimitedStringLength
+
 using BillingDesk.Subscription.Types.Enums;
 using NodaTime;
 
@@ -7,16 +8,11 @@ namespace BillingDesk.Subscription.Types.Models;
 public class Subscription
 {
 	public Guid Id { get; init; }
-
-	[StringLength(64)]
 	public required string Name { get; set; }
-
 	public decimal Cost { get; set; }
 	public Currency Currency { get; set; }
 	public BillingCycle BillingCycle { get; set; }
 	public LocalDate StartDate { get; set; }
 	public SubscriptionStatus Status { get; set; }
-
-	[StringLength(64)]
 	public required string Category { get; set; }
 }
